57178c01a675eac16b018ea5ae69c0c9648c57d1
Daniel Leech <dan.t.leech@gmail.com>
2022-06-26 11:28:25 +0200

    Fix CS

---
+++ tests/RuleExample/Workspace/features/example.feature
@@ -0,0 +1,5 @@
+Feature: Foobar
+    Scenario: This is a scenario
+        Given something
+        Then an exception should be thrown
+        When I do this
---
+++ tests/Util/Workspace.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DTL\GherkinLint\Tests\Util;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+
+class Workspace
+{
+    private string $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function putContents(string $path, string $contents): void
+    {
+        $path = $this->path($path);
+        if (!file_exists(dirname($path))) {
+            mkdir(dirname($path), 0744, true);
+        }
+        file_put_contents($path, $contents);
+    }
+
+    public function path(string $path): string
+    {
+        return Path::join($this->path, $path);
+    }
+
+    public function reset(): void
+    {
+        $fs = new Filesystem();
+        $fs->remove($this->path);
+    }
+
+}
