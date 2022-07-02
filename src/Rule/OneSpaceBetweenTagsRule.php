34530ea3af4d0128660ebdd3b22b87ab82a1e52f
Daniel Leech <dan.t.leech@gmail.com>
2022-07-02 15:59:24 +0200

    bugfix: Account tags on multiple lines

--- src/Rule/OneSpaceBetweenTagsRule.php
+++ src/Rule/OneSpaceBetweenTagsRule.php
@@ -84,13 +84,19 @@ class OneSpaceBetweenTagsRule implements Rule
     private function checkTags(array $tags): Generator
     {
         $lastEnd = null;
+        $currentLine = null;
         foreach ($tags as $tag) {
             if (null === $tag->location->column) {
                 continue;
             }
 
-            if ($lastEnd === null) {
+            if ($currentLine !== $tag->location->line) {
+                $currentLine = null;
+            }
+
+            if ($lastEnd === null || $currentLine === null) {
                 $lastEnd = $tag->location->column + mb_strlen($tag->name);
+                $currentLine = $tag->location->line;
                 continue;
             }
 
@@ -107,6 +113,7 @@ class OneSpaceBetweenTagsRule implements Rule
                     ),
                 );
             }
+
             $lastEnd = $tag->location->column + mb_strlen($tag->name);
         }
     }
